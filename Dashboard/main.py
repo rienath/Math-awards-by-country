284d6ccf5c388ffda94acb76a98d3ec6a32f8277
rienath <infernalion@hotmail.com>
2021-11-27 21:21:34 +0000

    Number stories 2 and 3

--- Dashboard/main.py
+++ Dashboard/main.py
@@ -6,7 +6,7 @@
 # TODO number story 7
 # TODO layout
 # TODO colours
-# TODO theme/2021 disclaimer
+# TODO theme/2021/AbelField disclaimer
 
 
 #import dash
@@ -41,6 +41,25 @@ colors = {
 # Make the page wide
 st.set_page_config(layout="wide")
 
+# Bootstrap
+
+st.markdown(
+    """<head>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+  integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
+  integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+  <style>
+  body{
+      background-color: #fff;
+      font-size: 40px;
+  }
+
+  </style>
+</head>""", unsafe_allow_html=True
+)
+
 # =====================
 # ===== LOAD DATA =====
 # =====================
@@ -71,22 +90,12 @@ df_overview = pd.read_csv(location_overview)
 # ===== Intro =====
 # =================
 
-st.markdown(
-    """<head>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
-  integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
-  integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-  <style>
-  body{
-      background-color: #fff;
-      font-size: 40px;
-  }
-
-  </style>
-</head>""", unsafe_allow_html=True
-)
+st.markdown('''
+<div class="jumbotron text-center" style='background-color: #fff'>
+  <h1 style="margin: auto; width: 100%;">Mathematics Awards Interactive Dashboard</h1>
+  <h1 style="margin: auto; width: 100%;"></h1>
+</div>
+''', unsafe_allow_html=True)
 
 
 # ==========================
@@ -125,46 +134,65 @@ box_3_value_2 = 'with ' + str(top_country_result) + ' laureates'
 box_4_value_2 = 'with ' + str(top_country_capita_result) + ' laureates per million inhabitants'
 
 st.markdown('''
-<div class="jumbotron text-center" style='background-color: #fff'>
-  <h1 style="margin: auto; width: 100%;">Mathematics Awards Interactive Dashboard</h1>
-  <h1 style="margin: auto; width: 100%;"></h1>
-</div>
-<div class="jumbotron text-center"  style='padding: 0'>
-  <div class="row" style='background-color: #fff; width: 100%; margin: auto;'>
-    <div class="col-sm-3" style="font-size: 15px; background-color: '''+box_background_colour+'''; border-radius: 30px">
-      <p style='text-align: center; font-weight: 400; color: #000'>'''+box_1_sign+'''</p>
-      <p style='text-align: center; font-size: 50px; font-weight: bold; color: #000'>'''+box_1_value+'''</p>
+<div class="jumbotron text-center"  style='padding: 0;'>
+  <div class="row justify-content-md-center" style='background-color: #fff; width: 100%; margin: auto;'>
+    <div class="col-sm-2" style="font-size: 15px; vertical-align: bottom;">
+        <div style="background-color: '''+box_background_colour+'''; border-radius: 40px; vertical-align: bottom;">
+          <p style='text-align: center; font-weight: 400; color: #000; vertical-align: bottom;'>'''+box_1_sign+'''</p>
+          <p style='text-align: center; font-size: 50px; font-weight: bold; color: #000'>'''+box_1_value+'''</p>
+          <p style='text-align: center; font-size: 0px; color: blue'>\00</p>
+        </div>
     </div>
-    <div class="col-sm-3" style='font-size: 15px; background-color: '''+box_background_colour+'''; border-radius: 30px'>
-      <p style='text-align: center; font-weight: 400; color: #000'>'''+box_2_sign+'''</p>
-      <p style='text-align: center; font-size: 50px; font-weight: bold; color: #e73631'>'''+box_2_value+'''</p>
+    <div class="col-sm-2" style='font-size: 15px;'>
+        <div style="background-color: '''+box_background_colour+'''; border-radius: 40px; vertical-align: middle;">
+          <p style='text-align: center; font-weight: 400; color: #000'>'''+box_2_sign+'''</p>
+          <p style='text-align: center; position: relative; vertical-align: center; font-size: 50px; font-weight: bold; color: #e73631'>'''+box_2_value+'''</p>
+          <p style='text-align: center; font-size: 0px; color: blue'>\00</p>
+        </div>
     </div>
-    <div class="col-sm-3" style='font-size: 15px; background-color: '''+box_background_colour+'''; border-radius: 30px'>
-      <p style='text-align: center; font-weight: 400; color: #000'>'''+box_3_sign+'''</p>
-      <p style='text-align: center; font-size: 35px; font-weight: bold; color: #70a82c'>'''+box_3_value+'''</p>
-      <p style='text-align: center; font-size: 15px; color: #70a82c'>'''+box_3_value_2+'''</p>
+    <div class="col-sm-3" style='font-size: 15px;'>
+        <div style="background-color: '''+box_background_colour+'''; border-radius: 40px; vertical-align: middle;">
+          <p style='text-align: center; font-weight: 400; color: #000'>'''+box_3_sign+'''</p>
+          <p style='text-align: center; font-size: 35px; font-weight: bold; color: #70a82c'>'''+box_3_value+'''</p>
+          <p style='text-align: center; font-size: 15px; color: #70a82c'>'''+box_3_value_2+'''</p>
+        </div>
     </div>
-    <div class="col-sm-3" style='font-size: 15px; background-color: '''+box_background_colour+'''; border-radius: 30px'>
-      <p style='text-align: center; font-weight: 400; color: #000'>'''+box_4_sign+'''</p>
-      <p style='text-align: center; font-size: 35px; font-weight: bold; color: blue'>'''+box_4_value+'''</p>
-      <p style='text-align: center; font-size: 15px; color: blue'>'''+box_4_value_2+'''</p>
+    <div class="col-sm-3" style='font-size: 15px;'>
+        <div style="background-color: '''+box_background_colour+'''; border-radius: 40px">
+          <p style='text-align: center; font-weight: 400; color: #000'>'''+box_4_sign+'''</p>
+          <p style='text-align: center; font-size: 35px; font-weight: bold; color: blue'>'''+box_4_value+'''</p>
+          <p style='text-align: center; font-size: 15px; color: blue'>'''+box_4_value_2+'''</p>
+        </div>
     </div>
   </div>
 </div>
 ''', unsafe_allow_html=True)
 
 
-# ==========================
-# ===== Number Story 2 =====
-# ==========================
+# =============================
+# ===== Number Story 2 & 3=====
+# =============================
 # > Bar plot race total
-
-
-
-# ==========================
-# ===== Number Story 3 =====
-# ==========================
 # > Bar plot race per capita
+
+st.markdown(
+    '''
+    <h1 style="margin: auto; width: 100%;"></h1>
+    <div class="row" style='background-color: #fff; width: 100%; margin: auto;'>
+        <div class="col-sm-6" style="font-size: 15px; border-radius: 30px">
+            <iframe src='https://flo.uri.sh/visualisation/7956000/embed' frameborder='0' scrolling='no' 
+                style='width:100%; height:600px;'> </iframe>
+            <div style='width:100%!; margin-top:4px!important; text-align:right!important;'></div>
+        </div>
+        <div class="col-sm-6" style="font-size: 15px; border-radius: 30px">
+            <iframe src='https://flo.uri.sh/visualisation/7956357/embed' frameborder='0' scrolling='no' 
+                style='width:100%; height:600px;'></iframe>
+            <div style='width:100%!; margin-top:4px!important; text-align:right!important;'></div>
+        </div>
+     </div>
+    ''',
+    unsafe_allow_html=True
+)
 
 
 # ==========================
